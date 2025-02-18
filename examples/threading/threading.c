685fdf59cd7cd4f596c88c4b40ffa02d7b7f620e
pmousoul <pmousoul@gmail.com>
2025-02-18 23:29:02 +0200

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,28 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    pthread_mutex_t* mutex = thread_func_args->mutex;
+    int wait_to_obtain_ms = thread_func_args->wait_to_obtain_ms;
+    int wait_to_release_ms = thread_func_args->wait_to_release_ms;
+
+    // wait
+    usleep(wait_to_obtain_ms);
+
+    // obtain mutex
+    if (pthread_mutex_lock(mutex) != 0){
+        thread_func_args->thread_complete_success = false;
+    }
+
+    // wait
+    usleep(wait_to_release_ms);
+
+    // release mutex
+    if (pthread_mutex_unlock(mutex) != 0){
+        thread_func_args->thread_complete_success = false;
+    }
+
     return thread_param;
 }
 
@@ -28,6 +50,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+
+    // setup thread attribute as joinable
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    // allocate memory for thread_data
+    struct thread_data* thread_param = (struct thread_data*) malloc(sizeof(struct thread_data));
+
+    // setup mutex and wait arguments
+    thread_param->mutex = mutex;
+    thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_param->wait_to_release_ms = wait_to_release_ms;
+    thread_param->thread_complete_success = true;
+
+    // pass thread_data to created thread using threadfunc() as entry point
+    if (pthread_create(thread, &attr, threadfunc, (void *)thread_param) == 0) {
+        return true;
+    }
+
     return false;
 }
 
