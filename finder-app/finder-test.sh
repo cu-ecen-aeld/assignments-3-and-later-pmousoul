0b44acb8af6e36a8585333ee8bf0762245e9e63e
pmousoul <pmousoul@gmail.com>
2025-02-21 17:46:12 +0200

    assignment-4-part-2 related

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONF=/etc/finder-app/conf
+username=$(cat ${CONF}/username.txt)
 OUTPUTFILE=/tmp/assignment4-result.txt
 
 if [ $# -lt 3 ]
@@ -33,7 +34,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONF}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
